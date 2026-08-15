f4a3d59a9ad54f3208ba3ffbae4a08009d1cc4a9
Malak3333 <malak.mohamed5@icloud.com>
2026-08-15 19:34:37 +0200

    utils fixad

--- assignment4/js/utils.js
+++ assignment4/js/utils.js
@@ -1,12 +1,23 @@
-export async function getHouses() {
-     try {
-          const response = await fetch("../data/houses.json");
-          if (!response.ok) throw new Error("Kunde inte hämta huset");
+export function getScareText(level) {
+     if (level === 1) {
+          return "Inte illa"
+     } else if (level === 2) {
+          return "Lite läskigt"
+     } else if (level === 3) {
+          return "Ganska läskigt"
+     } else if (level === 4) {
+          return "Obehagligt"
+     } else if (level === 5) {
+          return "Hemskt läskigt"
+     } else {
+          return "Okänd nivå"
+     }
+}
 
-          const data = await response.json();
-          return data
-     } catch (error) {
-          throw error;
-     
+export function showError(message) {
+     const errorDiv = document.getElementById("error-container");
+
+     if (errorDiv) {
+          errorDiv.innerHTML = "<p class='error-message'>" + message + "</p>";
      }
 }
